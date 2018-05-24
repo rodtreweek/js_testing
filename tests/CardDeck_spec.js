cc31eca5e65b493b009144f20e83176033a2a28a
Rod Treweek <rtreweek@gmail.com>
2018-05-23 21:09:52 -0700

    Fixing up redundancy

--- tests/CardDeck_spec.js
+++ tests/CardDeck_spec.js
@@ -2,12 +2,9 @@ describe("Show the deck", function() {
 
   it("Should show the deck", function() {
     expect(playingCards.prototype.init).not.toBeNull();
-
-describe ("Check html", function() {
+  });
 
   it("Should show us some html", function() {
     expect(playingCards.card.prototype.getHTML).not.toBeNull();
-    });
-    });
-});
+  });
 });
