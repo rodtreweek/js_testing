b82b17cd8e0a3417906984e1cc05a80a044b5f57
Treweek, RodneyX <rodneyx.treweek@intel.com>
2018-05-23 19:04:44 -0700

    Trying something else

--- tests/checkCardDeckSpread.js
+++ tests/checkCardDeckSpread.js
@@ -2,6 +2,6 @@ describe("Show the deck", function() {
   var cardDeck = playingCards();
 
   it("Should show the deck", function() {
-    expect(cardDeck.spread).toBeDefined();
+    expect(cardDeck.spread).toBeTruthy();
     });
 });
