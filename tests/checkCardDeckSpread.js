51240e2b132e1253d5d2e624f5c5adf1cbb81953
Rod Treweek <rtreweek@gmail.com>
2018-05-23 21:00:10 -0700

    Adding another assertion

--- tests/checkCardDeckSpread.js
+++ tests/checkCardDeckSpread.js
@@ -2,5 +2,11 @@ describe("Show the deck", function() {
 
   it("Should show the deck", function() {
     expect(playingCards.prototype.init).not.toBeNull();
+
+describe ("Check html", function() {
+
+  it("Should show us some html", function() {
+    expect(playingCards.card.prototype.getHTML).not.toBeNull();
+    });
     });
 });
